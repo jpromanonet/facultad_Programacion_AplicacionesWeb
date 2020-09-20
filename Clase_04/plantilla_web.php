bc8bfe651901950c71930bfb43cfb4580af827c5
jpromanonet <programming@jpromano.net>
2020-09-20 18:38:11 -0300

    Clase 04

---
+++ Clase_04/plantilla_web.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Plantilla Web</title>
+
+    <!-- CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">  
+
+</head>
+<body class="container">
+    <br>
+    <div class="alert alert-primary" role="alert">
+        Aplicaciones Web 2020.
+    </div>
+
+    <br>    
+    <div class="spinner-border" role="status">
+        <span class="sr-only">Loading...</span>
+    </div>
+
+
+    <!-- JavaScript -->
+    <script src="jquery/jquery-3.5.1.slim.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+
+</body>
+</html>
+
+
