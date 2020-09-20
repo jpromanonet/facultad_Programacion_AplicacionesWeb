1e4c1bd8658554ee761766b2f34e330b33d79f44
jpromanonet <programming@jpromano.net>
2020-09-20 17:49:33 -0300

    Clase 03

--- Clase_03/index.php
+++ Clase_03/index.php
@@ -1,3 +1,8 @@
 <?php
+    // Esto es un echo
     echo '<h1>Hola Mundo.</h1>';
+    // Ahora vienen los print
+    // Print con paréntesis.
+    print("Hola mundo con comillas dobles.");
+    print('Hola mundo con comillas simples.');
 ?>
