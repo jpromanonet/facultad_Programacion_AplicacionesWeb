831714d04336e8510a205c50c2a1d7c28d82f07c
Juan P. Romano <programming@jpromano.net>
2020-09-29 18:58:44 -0300

    Clase 06

---
+++ Clase_06/index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Inicio</title>
+
+    <!-- CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet"> 
+
+     <!-- Archivos a incluir -->
+     <?php  include('includes/menu.php');  ?>
+
+</head>
+<body class="container">
+    <?php menu(); ?>
+
+    <h1>Este es el INDEX</h1>
+
+    <!-- JavaScript -->
+    <script src="jquery/jquery-3.5.1.slim.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+
+</body>
+</html>
+
+
